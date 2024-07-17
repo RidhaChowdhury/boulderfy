94c7ebea301aefd203bbe1f52525bbfec3c8d599
Ridha Chowdhury <rac6115@psu.edu>
2024-07-17 02:39:25 -0500

    route colors on cards

--- screens/constants.ts
+++ screens/constants.ts
@@ -13,3 +13,16 @@ export type Route = {
     send: '#99D3B4',
   };
   
+  export const gradeColors: { [key: string]: string } = {
+    V0: '#FF1493',  // Deep Pink
+    V1: '#1E90FF',  // Dodger Blue
+    V2: '#008000',  // Lime Green
+    V3: '#32CD32',  // Green
+    V4: '#FF4500',  // Orange Red
+    V5: '#8B0000',  // Dark Red
+    V6: '#FF0000',  // Red
+    V7: '#A9A9A9',  // Dark Grey
+    V8: '#FFFFFF',  // White
+    V9: '#696969',  // Dim Grey
+    V10: '#000000', // Black
+  };
